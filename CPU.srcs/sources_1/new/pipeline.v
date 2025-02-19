8beb2466eda5b099ea9acd37efe6be25119a7b6b
BennyNTHU <benny@icg.com.tw>
2025-02-20 04:14:49 +0800

    first commit

--- CPU.srcs/sources_1/new/pipeline.v
+++
@@ -1,9 +0,0 @@
-module Pipeline_Register (
-    input clk,
-    input [63:0] in_data,
-    output reg [63:0] out_data
-);
-    always @(posedge clk) begin
-        out_data <= in_data;
-    end
-endmodule
